63df7c07c73faa3fc07c8a9138e8d75deefc229a
Nick <njc413@aim.com>
2015-10-26 20:04:40 +0000

    Add success page

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
     url(r'^message/create/$', MessageCreateView.as_view(), name='message_create'),
+    url(r'^success/$', Success.as_view(), name="success"),
 )
